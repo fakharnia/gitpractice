c7feb8199dcc5c9cc3e36bea94ebe3b3e08dbc30
fakharnia <fakharnia.2m@gmail.com>
2023-08-07 14:45:45 +0330

    Initail the project tree

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3500;
+
+
+app.get("/", (request, response, next) => response.json({ "message": "There is nothing for you kiddo!" }));
+
+app.listen(PORT, () => console.log("Server Run on Port 3500!"))
